fb4d3631164980fbbef323e0034948a648c51de1
paryzdo20 <paryzdo20@sps-prosek.cz>
2022-12-07 15:06:50 +0100

    Přidejte soubory projektu.

---
+++ BlackJack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BlackJack;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Vítej, jsi nový hráč nebo už tady máš účet?(1/2) ");
+            string start = Console.ReadLine();
+            bool status = true;
+
+            Console.WriteLine("Napiš zde svoje jméno: ");
+            string name = Console.ReadLine();
+
+            Game game = new Game(start, status, name);
+
+            game.Login();
+
+            game.Menu();
+
+
+        }
+    }
+}
---
+++ BlackJack/game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Game
+    {
+        internal bool Status;
+        internal string Continue;
+        internal string Name;
+        internal Game(string _continue, bool _status, string _name) 
+        {
+            Status = _status;
+            Continue = _continue;
+            Name = _name;
+        }
+
+        internal void Login()
+        {
+            bool validation = true;
+
+            while (validation)
+            {
+                if (Continue == "1")
+                {
+                    Console.WriteLine("Vítáme tě tu znovu " + Name);
+                    validation = false;
+                }
+                else if (Continue == "2")
+                {
+                    Console.WriteLine("Vítej " + Name);
+                    validation = false;
+                }
+                else
+                {
+                    Console.WriteLine("Vybral jsi špatnou možnost, napiš prosím pouze 1 nebo 2! ");
+                    Continue = Console.ReadLine();
+                }
+            }
+        }
+
+        internal void Menu()
+        {
+            while (Status)
+            {
+                Console.WriteLine("1: začít novou hru\n 2: žebříček \n 3: pravidla \n 4: konec hry");
+                string menu = Console.ReadLine();
+
+                if (menu != null)
+                {
+                    if (menu == "1")
+                    {
+                        Console.WriteLine("hra");
+                    }
+                    else if (menu == "2")
+                    {
+                        Console.WriteLine("žebříček");
+                    }
+                    else if (menu == "3")
+                    {
+                        Console.WriteLine("Základní princip hry je, že hráč chce mít hodnotu karet blíže 21 než krupiér, " +
+                            "\nale přitom 21 nepřekročit. Vyhrává ten, kdo má po ukončení hry v ruce nejvyšší součet, " +
+                            "\naniž by překročil 21. Hráč, který má v ruce součet karet větší než 21, je takzvaně " +
+                            "\n„trop“ neboli „přes“.");
+                    }
+                    else if (menu == "4")
+                    {
+                        Console.WriteLine("Konec hry");
+                        Status = false;
+                    }
+                }
+            }
+        }
+    }
+}
